21abb1b8fd48eed5bdf1db0bd1ede5994d1af5b6
Steve De Zitter <de.zitter.steve@gmail.com>
2019-09-16 22:42:23 +0200

    initial commit

---
+++ src/main/java/be/stdz/winecellar/ServletInitializer.java
@@ -0,0 +1,13 @@
+package be.stdz.winecellar;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WineCellarApplication.class);
+	}
+
+}
